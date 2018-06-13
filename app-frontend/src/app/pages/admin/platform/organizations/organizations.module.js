0ce7669763d7665af7db7e4561bf467f7f66aee4
Lucien Knechtli <lknechtli@gmail.com>
2018-06-13 13:37:11 -0400

    Add search back to org list view

--- app-frontend/src/app/pages/admin/platform/organizations/organizations.module.js
+++ app-frontend/src/app/pages/admin/platform/organizations/organizations.module.js
@@ -18,7 +18,7 @@ class PlatformOrganizationsController {
         }
         this.fetching = true;
 
-        let debouncedSearch = _.debounce(
+        this.debouncedSearch = _.debounce(
             this.onSearch.bind(this),
             500,
             {leading: false, trailing: true}
@@ -29,7 +29,7 @@ class PlatformOrganizationsController {
         });
         this.currentUgrPromise = this.$scope.$parent.$ctrl.currentUgrPromise;
         this.getPlatUgrs();
-        this.$scope.$watch('$ctrl.search', debouncedSearch);
+        this.debouncedSearch();
     }
 
     getPlatUgrs() {
